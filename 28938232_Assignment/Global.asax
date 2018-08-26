ce3058ca362da27de220f07f7d468c504fc0dfcb
Aravind <aravind.cyber@yahoo.co.in>
2018-08-26 11:39:08 +1000

    Add project files.

---
+++ 28938232_Assignment/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_28938232_Assignment.MvcApplication" Language="C#" %>
---
+++ 28938232_Assignment/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_28938232_Assignment.Startup))]
+namespace _28938232_Assignment
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
